82d617759e40557dd25533eac050d92fbf1a61ca
n-OlegS <oleg@73rus.com>
2022-08-18 14:57:01 +0400

    More test cases for manufacturing

--- tests/test_commands.py
+++ tests/test_commands.py
@@ -14,8 +14,9 @@ class MyTestCase(unittest.TestCase):
         return state
 
     def test_manufacture(self):
-        class_tup = parse(self.return_state())
+        # TEST CASE 1
 
+        class_tup = parse(self.return_state())
         player = class_tup[0][1]
         cache = class_tup[1]
         player.money = 1
@@ -29,10 +30,42 @@ class MyTestCase(unittest.TestCase):
         self.assertEqual(cache.containers["0"], 9)
         self.assertEqual(cache.containers["1"], 8)
         self.assertEqual(cache.containers["2"], 9)
-        self.assertEqual(player.port.factoryShop.total_items, 3)
+        self.assertEqual(player.port.factoryShop.total_items(), 3)
         self.assertEqual(len(player.port.factoryShop.items["2"]), 1)
         self.assertEqual(len(player.port.factoryShop.items["1"]), 2)
 
+        # TEST CASE 2
+
+        class_tup = parse(self.return_state())
+        player = class_tup[0][1]
+        cache = class_tup[1]
+        p2 = class_tup[0][0]
+
+        self.assertEqual(player.manufacture(), 1)
+
+        player.port.plants = {"0": 1, "1": 1, "2": 1, "3": 0, "4": 0}
+        player.port.factoryShop.items["1"] = [0, 0, 1, 2, 2]
+        player.money = 1
+
+        self.assertEqual(player.manufacture(), 1)
+        self.assertEqual(player.manufacture(preffered=[0, 1]), 1)
+        self.assertEqual(player.manufacture(preffered=[0, 3]), 1)
+
+        cache.containers["0"] = 0
+
+        self.assertEqual(player.manufacture(preffered=[0, 1]), 1)
+        self.assertEqual(player.manufacture(preffered=[0]), 1)
+        self.assertEqual(player.manufacture(preffered=[3]), 1)
+
+        # TEST CASE 3
+
+        class_tup = parse(self.return_state())
+        player = class_tup[0][1]
+        cache = class_tup[1]
+        p2 = class_tup[0][0]
+        player.money = 1
+
+        self.assertEqual(player.manufacture(preffered=[1]), 0)
 
 if __name__ == '__main__':
     unittest.main()
